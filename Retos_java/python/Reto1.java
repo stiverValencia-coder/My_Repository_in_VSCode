c6d7e6c658e0fa81534a619afd77fa63c6143a24
Stiver Gerrard Valencia Marin <stivergerarrardvalenciamarin@gmail.com>
2026-05-03 23:34:57 -0600

    Aprendizaje de programacion dia 1

---
+++ Retos_java/python/Reto1.java
@@ -0,0 +1,24 @@
+package Retos_java.python;
+
+public class Reto1 {
+    public static void main(String[] args) {
+        String nombre = "Stiver";
+        int edad = 20;
+        double estatura = 1.75;
+        boolean estudianteProgramacion = true;
+        
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Estatura: " + estatura);
+        System.out.println("¿Estudia programacion? "+ estudianteProgramacion);
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println("Suma: " + (a + b));
+        System.out.println("Resta: " + (a - b));
+        System.out.println("Multiplicacion: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Residuo: " + (a % b));
+    }
+}
